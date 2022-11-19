e118185c1fde1e3ebdc2fdbdfe0019c9762d2c61
nimendoza <ncisomendoza@gmail.com>
2022-11-19 18:19:38 +0800

    Made bare minimum user demo for web-server

--- web-server/src/routes/index.ts
+++ web-server/src/routes/index.ts
@@ -1,11 +1,18 @@
 import { Router } from 'express';
 
+import fs from 'fs';
 import notFound from 'routes/pages/404';
 import root from 'routes/pages/root';
 import V1 from 'routes/pages/V1';
+import swaggerUi, { JsonObject } from 'swagger-ui-express';
+import jsyaml from 'js-yaml';
 
 const router = Router();
 router.use('/V1', V1);
+
+router.use('/docs', swaggerUi.serve);
+router.get('/docs', swaggerUi.setup(jsyaml.load(fs.readFileSync('src/types/openapi.yaml', 'utf8')) as JsonObject));
+
 router.use(notFound);
 router.use(root);
 export default router;
